c9d9480474138a347f679743731c3afc3a8b81c1
ale <alessandro.diblasi@capgemini.com>
2020-04-26 17:20:22 +0200

    add implement signup

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -3,6 +3,43 @@ import {AUTH_URL} from './../config/config'
 
 function Auth() {
 
+    const manageResult = (result) => {
+        const data = result['data'];
+        if(!data || !data['access_token']){
+            return Promise.reject('Invalid server response');
+        }
+        const expireDate = (new Date()).getTime() + data['expires_in']*1000;
+        localStorage.setItem('token-expires', expireDate)
+        localStorage.setItem('auth', JSON.stringify(result.data))
+        return result.data;
+    }
+
+    const isTokenExpired = () => {
+        const expired = +localStorage.getItem('token-expires');
+        const res = (new Date())> expired;
+        if(res) {
+            localStorage.removeItem('token-expires');
+            localStorage.removeItem('auth');
+        }
+        return res;
+    }
+
+    const handleError = (resp) => {
+        let message = '';
+        switch(+resp.status){
+            case 401: 
+                message = resp.data.error;
+                break;
+            case 500:
+                 message = resp.data.message;
+                break;
+            default:
+                message= 'Qualcosa è andato storto';
+                break;
+        }
+        return message;
+    }
+
     const addAxiosToken =() => {
         const token = getToken();
         if(token) {
@@ -17,16 +54,17 @@ function Auth() {
                 email,
                 password
             });
-            
-            localStorage.setItem('auth', JSON.stringify(result.data))
-            return result.data;
+            return manageResult(result);
         } catch (e) {
-            console.log(e);
-            return e;
+            console.log(e.response.data.error);
+            return Promise.reject(handleError(e.response));
         }
     };
 
     const getToken = ()=> {
+        if(isTokenExpired()){
+            return null;
+        }
         const auth = JSON.parse(localStorage.getItem('auth'));
         if(auth){
             return auth.access_token
@@ -36,6 +74,9 @@ function Auth() {
     }
 
     const getUser = () => {
+        if(isTokenExpired()){
+            return null;
+        }
         const auth = JSON.parse(localStorage.getItem('auth'));
         if(auth){
             return auth.user
@@ -44,16 +85,31 @@ function Auth() {
         }
     }
 
-    const signup = () => {};
+    const signup = async (email, name, password) => {
+        try {
+            const result = await axios.post( AUTH_URL + 'signup',
+            {
+                email: email,
+                name: name,
+                password: password
+            });
+            return manageResult(result);
+        } catch (e) {
+            console.log(e.response.data.error);
+            return Promise.reject(handleError(e.response));
+        }
+    };
 
     const logout = async () => {
         addAxiosToken();
         try {
             const result = await axios.post(AUTH_URL + 'logout');
             localStorage.removeItem('auth');
+            localStorage.removeItem('token-expires');
+
             return result;
         } catch(e) {
-            return e;
+            return Promise.reject(handleError(e.response));
         };
     };
 
@@ -64,7 +120,8 @@ function Auth() {
         signup,
         logout,
         refresh,
-        getUser
+        getUser,
+        isTokenExpired
     }
 }
 
--- src/components/login.js
+++ src/components/login.js
@@ -3,13 +3,25 @@ import Auth from './../auth/auth';
 import { UserDataContext} from '../containers/logincontext';
 
 
-const Login = (pars) => {
+const Login = (props) => {
+
+    const [name, setName] = useState('');
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [user, setUser] = React.useContext(UserDataContext);
 
+    const signupUser = (e) => {
+        e.preventDefault();
 
+        Auth.signup(email, name, password)
+        .then(payload => {
+            setUser(payload.user);
+            props.history.push('/');
+        }).catch(error => {
+            alert(error);
+        });
+    }
 
     const loginUser = (e) => {
         e.preventDefault();
@@ -17,17 +29,33 @@ const Login = (pars) => {
         Auth.signin(email, password)
         .then(payload => {
             setUser(payload.user);
-            pars.history.push('/');
+            props.history.push('/');
+        }).catch(error => {
+            alert(error);
         });
     }
 
     const resetForm = () => {
         setEmail('');
         setPassword('');
+        setName('');
     }
 
     return(
-        <form className="form login" onSubmit={loginUser}>
+        <form className="form login" onSubmit={props.signup ? signupUser : loginUser}>
+            <h1>{props.signup ? 'SIGN UP' : 'LOG IN'}</h1>
+
+            {props.signup &&
+            <div className="form-group">
+                <label htmlFor="name">User name</label>
+                <input required 
+                name="name" 
+                id="name" 
+                value={name} 
+                className="form-field"
+                onChange={(e) => setName(e.target.value)}/>
+            </div>}
+
             <div className="form-group">
                 <label htmlFor="email">Email</label>
                 <input required 
@@ -37,6 +65,7 @@ const Login = (pars) => {
                 className="form-field"
                 onChange={(e) => setEmail(e.target.value)}/>
             </div>
+
             <div className="form-group">
                 <label htmlFor="password">Password</label>
                 <input required 
@@ -48,7 +77,7 @@ const Login = (pars) => {
                 onChange={(e) => setPassword(e.target.value)}/>
             </div>
             <div className="form-group buttons">
-                <button className="btn btn-success">LOGIN</button>
+                <button className="btn btn-success">{props.signup ? 'SIGNUP' : 'LOGIN'}</button>
                 <button 
                 type="reset"
                 onClick={resetForm}
